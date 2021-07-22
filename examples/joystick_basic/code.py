0ffcbfed705745b1f8cff01cbb989caec3f55a34
Edward Wright <fasteddy@thewrightspace.net>
2021-07-22 17:02:12 -0400

    Updated examples

--- examples/joystick_basic/code.py
+++ examples/joystick_basic/code.py
@@ -1,39 +1,38 @@
 """
 JoystickXL basic example with 2 axes, 4 buttons, 1 hat switch.
 
-(Don't forget to also copy boot.py from the example folder to your CIRCUITPY drive!)
+Tested on an Adafruit ItsyBitsy M4 Express, but should work on other CircuitPython
+boards with a sufficient quantity/type of pins.
+
+* Buttons are on pins D9, D10, D11 and D12
+* Axes are on pins A2 and A3
+* Hat switch is on pins D2 (up), D3 (down), D4 (left) and D7 (right)
+
+Don't forget to copy boot.py from the example folder to your CIRCUITPY drive.
 """
 
-import analogio
-import board
-import digitalio
-from joystick_xl.helpers import Axis, Hat
+import board  # type: ignore (this is a CircuitPython built-in)
+from joystick_xl.helpers import Axis, Button, Hat
 from joystick_xl.joystick import Joystick
 
-joystick = Joystick()
+if __name__ == "__main__":
+    joystick = Joystick()
 
-buttons = [
-    digitalio.DigitalInOut(board.D2),
-    digitalio.DigitalInOut(board.D3),
-    digitalio.DigitalInOut(board.D4),
-    digitalio.DigitalInOut(board.D5),
-]
+    b1 = Button(board.D9)
+    b2 = Button(board.D10)
+    b3 = Button(board.D11)
+    b4 = Button(board.D12)
 
-for button in buttons:
-    button.direction = digitalio.Direction.INPUT
-    button.pull = digitalio.Pull.UP
+    x = Axis(board.A2)
+    y = Axis(board.A3)
+    h = Hat(up=board.D2, down=board.D3, left=board.D4, right=board.D7)
 
-x = Axis(analogio.AnalogIn(board.A2))
-y = Axis(analogio.AnalogIn(board.A3))
-h = Hat(up=board.D6, down=board.D7, left=board.D8, right=board.D9)
-
-while True:
-
-    for i, button in enumerate(buttons, start=1):
-        if button.value:
-            joystick.release_buttons(i)
-        else:
-            joystick.press_buttons(i)
-
-    joystick.move_hats(("h1", h.update()))
-    joystick.move_axes(("x", x.update()), ("y", y.update()))
+    while True:
+        joystick.update_button(
+            (0, b1.value),
+            (1, b2.value),
+            (2, b3.value),
+            (3, b4.value),
+        )
+        joystick.update_axis((0, x.value), (1, y.value))
+        joystick.update_hat((0, h.value))
--- examples/simple_test/code.py
+++ examples/simple_test/code.py
@@ -1,3 +1,13 @@
+"""
+JoystickXL simple test.
+
+No physical I/O required, just use a joystick/gamepad testing application (like the one
+built-in to Windows) and watch the inputs cycle.
+
+Tested on an Adafruit QT-Py, but should work on other CircuitPython boards.
+
+Don't forget to copy boot.py from the example folder to your CIRCUITPY drive.
+"""
 import time
 
 from joystick_xl.joystick import Joystick
@@ -7,21 +17,24 @@ if __name__ == "__main__":
 
     while True:
 
-        for i in range(1, js.num_buttons + 1):
-            js.press_buttons(i)
+        # Exercise every defined button
+        for i in range(js.num_buttons):
+            js.update_button((i, True))
             time.sleep(0.05)
-            js.release_buttons(i)
+            js.update_button((i, False))
             time.sleep(0.05)
 
+        # Exercise every defined axis
         for a in range(js.num_axes):
             for i in range(0, -128, -1):
-                js.move_axes((a, i))
+                js.update_axis((a, i))
             for i in range(-127, 128):
-                js.move_axes((a, i))
+                js.update_axis((a, i))
             for i in range(127, -1, -1):
-                js.move_axes((a, i))
+                js.update_axis((a, i))
 
+        # Exercise every defined hat switch
         for h in range(js.num_hats):
             for i in range(0, 9):
-                js.move_hats((h, i))
+                js.update_hat((h, i))
                 time.sleep(0.25)
