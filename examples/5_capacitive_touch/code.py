3b96e859d480dcf0cb4f241f310dce3e9b5bf5a6
Edward Wright <fasteddy@thewrightspace.net>
2021-08-13 22:50:15 -0400

    Update capacitive touch example

--- examples/5_capacitive_touch/code.py
+++ examples/5_capacitive_touch/code.py
@@ -1,11 +1,10 @@
 """
-JoystickXL capacitive touch example with 8 buttons and 1 hat switch.
+JoystickXL Example #5 - Capacitive Touch (8 buttons and 1 hat switch).
 
-MPR121 12-Key Capacitive Touch Sensor Breakout https://www.adafruit.com/product/1982
+This example uses an MPR121 12-Key Capacitive Touch Sensor Breakout https://www.adafruit.com/product/1982,
+and requires the `adafruit_mpr121` and `adafruit_bus_device` libraries from the CircuitPython Library Bundle.
 
-Requires the `adafruit_mpr121` and `adafruit_bus_device` libraries
-
-Tested on an Adafruit ItsyBitsy M4 Express, but should work on other CircuitPython
+Tested on an Adafruit Metro M4 Express, but should work on other CircuitPython
 boards with a sufficient quantity/type of pins.
 
 * 3V, G, SCL, SDA from CircuitPython board to MPR121 board
@@ -28,22 +27,20 @@ mpr121 = adafruit_mpr121.MPR121(i2c)
 # Set up JoystickXL!
 js = Joystick()
 
-# Add 8 buttons - no `source=Board.X` here, since we will be using the MPR121.
-for _ in range(8):
-    js.add_input(Button(active_low=False))
+# The MPR121 library returns True when a capacitive touch channel is activated.  This
+# makes it "active high", so we set `active_low` to False
+for i in range(8):
+    js.add_input(Button(mpr121[i], active_low=False))
 
-# Add a hat switch - again, no `Board.X` sources.
-js.add_input(Hat(active_low=False))
+js.add_input(
+    Hat(
+        up=mpr121[8],
+        down=mpr121[9],
+        left=mpr121[10],
+        right=mpr121[11],
+        active_low=False,
+    )
+)
 
 while True:
-
-    # Manually update the source values for our inputs using the MPR121.
-    for i in range(8):
-        js.button[i].source_value = mpr121[i].value
-    js.hat[0].up.source_value = mpr121[8].value
-    js.hat[0].down.source_value = mpr121[9].value
-    js.hat[0].left.source_value = mpr121[10].value
-    js.hat[0].right.source_value = mpr121[11].value
-
-    # Update the joystick device
     js.update()
